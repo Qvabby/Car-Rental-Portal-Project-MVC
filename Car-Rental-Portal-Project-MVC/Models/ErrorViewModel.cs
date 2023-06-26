613a395d5a1b63dd1d26d91efadc1f951b6359c8
WYBM <saluqvadze2006@gmail.com>
2023-06-26 20:39:55 +0400

    Add project files.

---
+++ Car-Rental-Portal-Project-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Car_Rental_Portal_Project_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
