31bf0eb0c122d4985496065b9bd7bd024eb29e67
WYBM <saluqvadze2006@gmail.com>
2023-06-27 18:02:12 +0400

    Added ServiceResponse class which will be used to return any kind of response from service methods.

---
+++ Car-Rental-Portal-Project-MVC/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Car_Rental_Portal_Project_MVC.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car_Rental_Portal_Project_MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _accountService;
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+    }
+}
---
+++ Car-Rental-Portal-Project-MVC/Services/ServiceResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Car_Rental_Portal_Project_MVC.Services
+{
+    public class ServiceResponse<T>
+    {
+        //This is the place where the Response Data will be stored.
+        public T? Data { get; set; }
+        //This is the string where Response Message will be stored.
+        public string Message { get; set; } = string.Empty;
+        //here you can store string that will tell in detail what the response did.
+        public string Description { get; set; } = string.Empty;
+        //here you can check if response was successful or not.
+        public bool success { get; set; } = true;
+
+    }
+}
