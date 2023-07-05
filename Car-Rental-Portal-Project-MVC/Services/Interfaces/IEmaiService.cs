f8ba47b6ec44ee421ff9892842be88588f78114a
Nika <135450683+nikusha43@users.noreply.github.com>
2023-07-05 17:32:02 +0400

    IEmailService

---
+++ Car-Rental-Portal-Project-MVC/Services/Interfaces/IEmaiService.cs
@@ -0,0 +1,7 @@
+﻿namespace Car_Rental_Portal_Project_MVC.Services.Interfaces
+{
+    public interface IEmaiService
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
