93056df1c4871345cd3d7de8a5c3f04be9b7efd1
Nika <135450683+nikusha43@users.noreply.github.com>
2023-07-10 15:25:03 +0400

    Wasnot Deleted

--- Car-Rental-Portal-Project-MVC/Services/Interfaces/ICarService.cs
+++ Car-Rental-Portal-Project-MVC/Services/Interfaces/ICarService.cs
@@ -5,19 +5,7 @@ namespace Car_Rental_Portal_Project_MVC.Services.Interfaces
 {
 	public interface ICarService
 	{
-		Task<ServiceResponse<CarFilterResultViewModel>> FilterByCity(string city);
-		Task<ServiceResponse<CarFilterResultViewModel>> FilterByReleaseYear(int startYear, int endYear);
-		Task<ServiceResponse<CarFilterResultViewModel>> FilterByCapacity(int capacity);
-		Task<ServiceResponse<CarFilterResultViewModel>> FilterByManufacturer(string manufacturer);
-		Task<ServiceResponse<CarFilterResultViewModel>> FilterByModel(string model);
-		Task<ServiceResponse<CarFilterResultViewModel>> FilterByPrice(decimal minPrice, decimal maxPrice);
-		Task<ServiceResponse<CarFilterResultViewModel>> FilterByEngine(float minEngine, float maxEngine);
-		Task<ServiceResponse<CarFilterResultViewModel>> FilterByTransmission(TransmissionEnum transmission);
-		Task<ServiceResponse<CarFilterResultViewModel>> FilterByFuelType(FuelTypeEnum fuelType);
-		Task<ServiceResponse<CarFilterResultViewModel>> FilterByFuelTank(int minFuelTank, int maxFuelTank);
-		Task<ServiceResponse<CarFilterResultViewModel>> FilterByWheelType(WheelTypeEnum wheelType);
-		Task<ServiceResponse<CarFilterResultViewModel>> FilterByLocation(string location);
-		Task<ServiceResponse<CarFilterResultViewModel>> FilterByAll(CarFilterParameters filterParameters);
+		
 	}
 
 }
