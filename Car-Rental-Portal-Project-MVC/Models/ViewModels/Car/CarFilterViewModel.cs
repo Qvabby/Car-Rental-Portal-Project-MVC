01ca7cdd10133a69f6c92802e919195d0ca00ac8
Nika <135450683+nikusha43@users.noreply.github.com>
2023-07-14 16:34:37 +0400

    +CarFilterViewModel, EnumDrop Downs

---
+++ Car-Rental-Portal-Project-MVC/Models/ViewModels/Car/CarFilterViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Car_Rental_Portal_Project_MVC.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Car_Rental_Portal_Project_MVC.Models.ViewModels.Car
+{
+    public class CarFilterViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Manufacturer { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Model { get; set; }
+        //1900-2023
+        [Required]
+        [Range(1900, 2023)]
+        public int Year { get; set; }
+        [Required]
+        [Range(0, 99999)]
+        public decimal Price { get; set; }
+        [Required]
+        public float Engine { get; set; }
+
+        [Required]
+        public TransmissionEnum Transmission { get; set; }
+
+        [Required]
+        public FuelTypeEnum FuelType { get; set; }
+
+
+        [Required]
+        public int FuelTank { get; set; }
+        [Required]
+        public WheelTypeEnum WheelType { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Location { get; set; }
+        [Required]
+        public int PeopleAmount { get; set; }
+
+    }
+}
